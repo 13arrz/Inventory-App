b80dd91ca650af50ddd81eaeae7b50929190284a
Josh <jxm5210@g.rit.edu>
2018-08-07 23:15:27 -0400

    Add LoadingScreen

---
+++ InventoryApp/screens/LoadingScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+
+export default class LoadingScreen extends React.Component {
+	componentDidMount() {
+		// determine whether there are profiles saved or not
+		// if so, send the user to the profiles screen
+		// if not, send the user to create a new profile
+	}
+	
+	render() {
+		<View style={styles.container}>
+			<Text>Loading...</Text>
+			<ActivityIndicator size='large'>
+		</View>
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+})
