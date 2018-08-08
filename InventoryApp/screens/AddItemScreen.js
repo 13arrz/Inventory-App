8d1a08cc10eeed6defc570cbaac6a865989bd780
Josh <jxm5210@g.rit.edu>
2018-08-08 03:54:25 -0400

    Add GUI to AddItemScreen

--- InventoryApp/screens/AddItemScreen.js
+++ InventoryApp/screens/AddItemScreen.js
@@ -9,8 +9,21 @@ export default class AddItemScreen extends React.Component {
 		this.state = {
 			navigation: props.navigation,
 			profiles: props.navigation.getParam('profiles', null),
-			curProfile: props.navigation.getParam('curProfile', null)
+			curProfile: props.navigation.getParam('curProfile', null),
+			error: null,
+			itemName: null,
+			itemQuantity: "0"
 		}
+	}
+	
+	addItem = () => {
+		var newProfiles = this.state.profiles;
+		
+		// error checking
+		
+		// ======
+		
+		
 	}
 	
 	render() {
@@ -27,7 +40,57 @@ export default class AddItemScreen extends React.Component {
 					}}}
 					centerComponent={{text: 'Add Item', style: {color: '#fff'}}}
 				/>
+				
+				<KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+					{this.state.error &&
+						<Text style={{color: 'red', fontSize: 16}}>{this.state.error}</Text>
+					}
+					<Text>Enter new item name:</Text>
+					<TextInput
+						style={styles.textInput}
+						onChangeText={(itemName) => this.setState({itemName})}
+						value={this.state.itemName}
+					/>
+					<Text>Enter initial item quantity:</Text>
+					<TextInput
+						style={styles.textInput}
+						onChangeText={(itemQuantity) => this.setState({itemQuantity})}
+						value={this.state.itemQuantity}
+						keyboardType="numeric"
+					/>
+					<TouchableOpacity
+						style={styles.submitButton}
+						onPress={this.addItem}
+					>
+						<Text>Add</Text>
+					</TouchableOpacity>
+				</KeyboardAvoidingView>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	textInput: {
+		height: 40,
+		width: '80%',
+		backgroundColor: '#fff',
+		opacity: .9,
+		borderColor: 'gray',
+		borderWidth: 1,
+		marginTop: 10
+	},
+	submitButton: {
+		backgroundColor: 'lightblue',
+		marginTop: 10,
+		height: 40,
+		width: 100,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+})
