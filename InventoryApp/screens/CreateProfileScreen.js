fca66150339e659d13a4cab6a39703a0b7d8b81c
Josh <jxm5210@g.rit.edu>
2018-08-07 23:49:44 -0400

    Add CreateProfileScreen GUI

--- InventoryApp/screens/CreateProfileScreen.js
+++ InventoryApp/screens/CreateProfileScreen.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 export default class CreateProfileScreen extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			profileName: null
+		}
+	}
+	
+	createProfile = () => {
+		console.log("Create new profile");
+	}
+	
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>CreateProfileScreen</Text>
+				<Text>Enter new profile name:</Text>
+				<TextInput
+					style={styles.textInput}
+					onChangeText={(profileName) => this.setState({profileName})}
+					value={this.state.profileName}
+				/>
+				<TouchableOpacity
+					style={styles.submitButton}
+					onPress={this.createProfile}
+				>
+					<Text>Create</Text>
+				</TouchableOpacity>
 			</View>
 		)
 	}
@@ -16,5 +39,22 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	textInput: {
+		height: 40,
+		width: '80%',
+		backgroundColor: '#fff',
+		opacity: .9,
+		borderColor: 'gray',
+		borderWidth: 1,
+		marginTop: 10
+	},
+	submitButton: {
+		backgroundColor: 'lightblue',
+		marginTop: 10,
+		height: 40,
+		width: 100,
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 })
