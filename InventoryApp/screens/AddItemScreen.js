37f2c0f0849d10a54ce0911a9e0af607cca1214e
Josh <jxm5210@g.rit.edu>
2018-08-08 03:42:16 -0400

    Add header to AddItemScreen

--- InventoryApp/screens/AddItemScreen.js
+++ InventoryApp/screens/AddItemScreen.js
@@ -7,11 +7,27 @@ export default class AddItemScreen extends React.Component {
 		super(props);
 		
 		this.state = {
-			navigation: props.navigation
+			navigation: props.navigation,
+			profiles: props.navigation.getParam('profiles', null),
+			curProfile: props.navigation.getParam('curProfile', null)
 		}
 	}
 	
 	render() {
-		return <View />
+		return (
+			<View style={{flex: 1}}>
+				<Header
+					leftComponent={{icon: 'keyboard-arrow-left', color: '#fff', onPress: () => {
+						// cancel the request to add a new item
+						this.props.navigation.navigate("ProfileScreen", 
+						{ 
+							profiles: this.state.profiles,
+							curProfile: this.state.curProfile
+						});
+					}}}
+					centerComponent={{text: 'Add Item', style: {color: '#fff'}}}
+				/>
+			</View>
+		)
 	}
 }
